6b7731d9172aae02da00b082851a83ded618e8bd
hc2su <hc2su@nettop169.cs.uwaterloo.ca>
2019-06-02 06:17:45 -0400

    s

--- backend/predictions/predict.py
+++ backend/predictions/predict.py
@@ -56,7 +56,7 @@ x_val[x_val.columns] = scaler.transform(x_val)
 #print(x_val)
 # I is number of 10 seconds ahead we are predicting
 for i in range(1, numberOf10Seconds):
-  temp = np.reshape(np.array(x_val.tail(x_val.shape[0])),(1,x_val.shape[0],x_val.shape[1]))
+  temp = np.reshape(np.array(x_val.iloc[1:].tail(x_val.shape[0])),(1,x_val.shape[0],x_val.shape[1]))
   predictions = model.predict(temp)
   mostRecent = x_val.tail(1)
   print(mostRecent)
