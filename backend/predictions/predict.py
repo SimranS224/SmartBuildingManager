a0434aa3e1e4781a75429509b13c26a918e68a24
hc2su <hc2su@nettop169.cs.uwaterloo.ca>
2019-06-02 06:21:17 -0400

    s

--- backend/predictions/predict.py
+++ backend/predictions/predict.py
@@ -52,11 +52,11 @@ x_val.sort_values(["date", "roomId"],inplace=True)
 x_val = x_val.groupby("date").apply(transformInput).reset_index(level=0)
 print(x_val)
 scaler = joblib.load(scaler_filename)
-x_val[x_val.iloc[1:].columns] = scaler.transform(x_val.iloc[1:])
+x_val[x_val.iloc[:,1:].columns] = scaler.transform(x_val.iloc[:,1:])
 #print(x_val)
 # I is number of 10 seconds ahead we are predicting
 for i in range(1, numberOf10Seconds):
-  temp = np.reshape(np.array(x_val.iloc[1:].tail(x_val.shape[0])),(1,x_val.shape[0],x_val.shape[1]))
+  temp = np.reshape(np.array(x_val.iloc[:,1:].tail(x_val.shape[0])),(1,x_val.shape[0],x_val.shape[1]))
   predictions = model.predict(temp)
   mostRecent = x_val.tail(1)
   print(mostRecent)
